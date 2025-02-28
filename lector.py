fd256bebac04a7aa89d8f2082901da9a21d8c3c6
Yamizaki <Arakaki_18@outlook.com>
2025-02-28 16:24:56 -0500

    A

--- lector.py
+++ lector.py
@@ -3,7 +3,7 @@ import pandas as pd
 def read_csv(name_csv):
     # Cargar el archivo Excel (reemplaza 'archivo.xlsx' con el nombre de tu archivo)
     df = pd.read_excel(f"uploads/{name_csv}", engine="openpyxl")
-
+    df.columns = df.columns.str.strip().str.lower()
     # Imprimir las columnas del DataFrame
-    nombres = df["NOMBRES Y APELLIDOS"].dropna().tolist()
+    nombres = df["nombres y apellidos"].dropna().tolist()
     return nombres
